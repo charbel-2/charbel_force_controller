bc45137e723fbc85bdccf36f3aef6ca8e0b6d62d
charbel-2 <131307737+charbel-2@users.noreply.github.com>
2026-04-02 16:38:52 +0200

    Delete scripts directory

--- scripts/install_libfranka.sh
+++
@@ -1,8 +0,0 @@
-git clone --recursive https://github.com/frankarobotics/libfranka.git
-cd libfranka
-git checkout 0.13.3
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j$(nproc)
-sudo make install
-sudo ldconfig
